c874f26cfdd05d778feb28b9be8d3ddafd2e584b
Shay Yaacoby <shay66@gmail.com>
2016-12-12 21:27:32 +0200

    add events

--- google-analytics.js
+++ google-analytics.js
@@ -6,14 +6,23 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-72801155-2', 'auto');
 ga('send', 'pageview');
 
-$(document).on('change', 'input', function(e) {
-    ga('send', 'event', 'input', 'input change', $(e.currentTarget).val());
-  });
-  
-$(document).on('click', 'button', function() {
-  ga('send', 'event', 'button', 'button click');
-});
 
-$(document).on('click', 'a', function() {
-  ga('send', 'event', 'button', 'link click');
-});
+$('.shiny-bound-output').on('shiny:error',function(e) {ga('send', 'event', 'error', 'error', $(e.currentTarget).val());});
+
+$(document).on('change', $('#procedure_name'         ), function(e) {ga('send', 'event', 'input', 'procedure_name'   , $(e.currentTarget).val());});
+$(document).on('change', $('#measure_selected'       ), function(e) {ga('send', 'event', 'input', 'measure_selected' , $(e.currentTarget).val());});
+$(document).on('change', $('#input_method'           ), function(e) {ga('send', 'event', 'input', 'input_method'     , $(e.currentTarget).val());});
+$(document).on('change', $('#upload_file'            ), function(e) {ga('send', 'event', 'input', 'upload_file'      , $(e.currentTarget).val());});
+$(document).on('change', $('#agree_contribute'       ), function(e) {ga('send', 'event', 'input', 'agree_contribute' , $(e.currentTarget).val());});
+$(document).on('change', $('#lab'                    ), function(e) {ga('send', 'event', 'input', 'lab'              , $(e.currentTarget).val());});
+$(document).on('change', $('#email'                  ), function(e) {ga('send', 'event', 'input', 'email'            , $(e.currentTarget).val());});
+$(document).on('change', $('#mult_adjust'            ), function(e) {ga('send', 'event', 'input', 'mult_adjust'      , $(e.currentTarget).val());});
+$(document).on('change', $('#table-metadata input'   ), function(e) {ga('send', 'event', 'input', 'table-metadata'   , $(e.currentTarget).val());});
+$(document).on('change', $('#table-gxl input'        ), function(e) {ga('send', 'event', 'input', 'table-gxl'        , $(e.currentTarget).val());});
+$(document).on('change', $('#table-groups-info input'), function(e) {ga('send', 'event', 'input', 'table-groups-info', $(e.currentTarget).val());});
+
+
+$(document).on('click', $('#reset_all')          , function() {ga('send', 'event', 'button', 'reset_all');});
+$(document).on('click', $('#load_example_button'), function() {ga('send', 'event', 'button', 'load_example_button');});
+$(document).on('click', $('#read_more')          , function() {ga('send', 'event', 'button', 'read_more');});
+$(document).on('click', 'a'                      , function() {ga('send', 'event', 'button', 'link click');});
